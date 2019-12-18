282161a307498b4302fcfb45d67229e30908ff9f
weijieGuo <guo.weijie@outlook.com>
2019-12-18 13:05:41 -0800

    add cube-ui

--- vue.config.js
+++ vue.config.js
@@ -47,6 +47,24 @@ module.exports = {
         })
       })
     }
+  },
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
   }
 }
 // chainWebpack (config) {
